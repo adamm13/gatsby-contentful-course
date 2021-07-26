99a431d916cd569c3ac8feced437491a674f5101
adam <13.adamm@gmail.com>
2021-07-26 17:15:37 -0400

    add contentful slug pages

---
+++ src/pages/{contentfulPage.slug}.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import {Layout} from 'components'
+
+export default function ContentfulPage() {
+    return (
+        <Layout>
+            <h1>Contentful Page</h1>
+        </Layout>
+    )
+}
