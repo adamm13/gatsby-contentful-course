9fa74b6b13d2ac33c176b5d9fd15c580d76b827e
adam <13.adamm@gmail.com>
2021-08-17 11:28:36 -0400

    Added files

---
+++ src/components/RichText/index.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import { documentToReactComponents } from '@contentful/rich-text-react-renderer'
+import { BLOCKS } from '@contentful/rich-text-types'
+import { Hero } from "components";
+
+export const RichText = ({raw, references, }) => {
+    const referencesMap = {}
+    references.forEach((reference) => {
+        referencesMap[reference.contentful_id] = reference;
+    });
+
+    const options = {
+        renderNode: {
+            [BLOCKS.EMBEDDED_ENTRY]: (node) => {
+                const data = referencesMap[node.data.target.sys.id];
+                switch (data.__typename) {
+                    case "ContentfulHero":
+                        return (
+                            <Hero
+                                heading={data.heading}
+                                subHeading={data.subHeading}
+                                backgroundImage={
+                                    data.backgroundImage.gatsbyImageData
+                                }
+                            />
+                        );
+                    default:
+                        return null;
+                }
+            }
+        }
+    }
+
+    return <div>{documentToReactComponents(JSON.parse(raw), options)}</div>
+}
--- src/pages/{contentfulPage.slug}.js
+++ src/pages/{contentfulPage.slug}.js
@@ -1,10 +1,39 @@
 import React from 'react'
-import {Layout} from 'components'
+import {Layout, RichText} from 'components'
+import { graphql } from 'gatsby'
+import { documentToReactComponents } from '@contentful/rich-text-react-renderer'
 
-export default function ContentfulPage() {
-    return (
+export default function ContentfulPage(props) {
+    console.log(props)
+    return ( 
         <Layout>
-            <h1>Contentful Page</h1>
+            {!!props.data.contentfulPage.pageContent}
+            <RichText 
+            references ={props.data.contentfulPage.pageContent.references}
+            raw={props.data.contentfulPage.pageContent.raw} 
+            />
         </Layout>
     )
 }
+
+export const query = graphql`
+    query pageQuery($id: String) {
+        contentfulPage(id: {eq: $id}) {
+        id
+        title
+        pageContent{
+            raw
+            references{
+                ... on ContentfulHero {
+                  __typename
+                  heading
+                  subHeading
+                  backgroundImage {
+                    gatsbyImageData(layout: FULL_WIDTH, placeholder: BLURRED)
+                  }
+                }
+              }
+        }
+        }
+    }
+`
